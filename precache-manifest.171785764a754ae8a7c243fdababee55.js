5c1f07bacd20cfca1843491bd68ff147d248abe7
smnjsph13 <65280825+smnjsph13@users.noreply.github.com>
2020-06-03 18:59:25 +0800

    Updates

---
+++ precache-manifest.171785764a754ae8a7c243fdababee55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66ee456fde1ca1408f5e17df7504a62",
+    "url": "/smnjsph13/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/smnjsph13/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ff120b86effefa134fd5",
+    "url": "/smnjsph13/robofriends/static/css/main.bb1fc098.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/smnjsph13/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smnjsph13/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff120b86effefa134fd5",
+    "url": "/smnjsph13/robofriends/static/js/main.a1a96267.chunk.js"
+  },
+  {
+    "revision": "599eff5803a0e5c2fb09",
+    "url": "/smnjsph13/robofriends/static/js/runtime-main.9bf32f6b.js"
+  },
+  {
+    "revision": "caaebe00442be4252aa3fe65cd1294c6",
+    "url": "/smnjsph13/robofriends/static/media/IRONMAN.caaebe00.woff"
+  }
+]);
